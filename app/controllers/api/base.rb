09da04d8082c7ff8f8d81cce3fec9dd8a6333066
kelishrestha <kylesth@gmail.com>
2016-11-13 18:38:22 +0545

    Require necessary files and add prefix for base controller.

--- app/controllers/api/base.rb
+++ app/controllers/api/base.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
-# require File.join(File.dirname(__FILE__), 'v1', 'base')
+require File.join(File.dirname(__FILE__), 'v1', 'base')
 module API
   # Base API class to mount multiple versions base classes
   class Base < Grape::API
+    prefix :api
+
+    # Helpers
     helpers do
       def logger
         AppLogger.logger
@@ -47,6 +50,6 @@ module API
       authenticate
     end
 
-    # mount API::V1::Base
+    mount API::V1::Base
   end
 end
